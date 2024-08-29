a92c956bfd12e8a10da813c7e9fe32a57fc80a62
milaaulia21 <165994151+milaaulia21@users.noreply.github.com>
2024-08-29 17:41:18 +0700

    Add files via upload

---
+++ ModulPertemuan_1-2/class-object.php
@@ -0,0 +1,23 @@
+<?php
+//Definisi Kelas
+class Mobil {
+    //Atribut atau Properties
+    public $merk;
+    public $warna;
+
+    //Constructor
+    public function __construct($merk, $warna) {
+        $this->merk=$merk;
+        $this->warna=$warna;
+    }
+
+    // Metode atau Function
+    public function deskripsi() {
+        return "Mobil ini adalah $this->merk berwarna $this->warna.";
+    }
+}
+
+// Instansiasi Objek 
+$mobil = new Mobil("Toyota", "Hitam");
+echo $mobil->deskripsi();
+?>
